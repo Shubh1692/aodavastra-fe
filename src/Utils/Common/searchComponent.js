11e6989754552f856076a693343fca87822cf5a5
deepika2007 <deepika200799@gmail.com>
2023-02-06 21:34:20 +0530

    Update bag page and add serach page

---
+++ src/Utils/Common/searchComponent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Box } from '@mui/material';
+import Layout from '../../Components/Layout';
+
+const SearchComponent = () => {
+   
+    return (
+        <Box sx={{ marginTop: '8pc' }}>
+
+        </Box>
+    )
+}
+export default Layout(SearchComponent);
